6f470c372bb25598456a7e3888ac79297fefa16c
Alex Forrence <aforren1@jhu.edu>
2018-01-03 22:01:09 -0500

    *much* more concise conversion from any type to bytes + endian handling

--- src/packing.cpp
+++ src/packing.cpp
@@ -1,4 +1,30 @@
-#include <array>
 #include "packing.hpp"
-#include "endian.hpp"
 
+void packing::reverse_range(unsigned char buffer[], int left, int right)
+{
+    while (left < right)
+    {
+        int temp = buffer[left];
+        buffer[left++] = buffer[right];
+        buffer[right--] = temp;
+    }
+}
+
+template <class T>
+void packing::num2bigendbytes(T num, unsigned char b[sizeof(T)])
+{
+    memcpy(b, &num, sizeof(T));
+    reverse_range(b, 0, sizeof(T) - 1);
+}
+
+template <class T>
+T packing::bigendbytes2num(unsigned char b[sizeof(T)])
+{
+    T num;
+    reverse_range(b, 0, sizeof(T) - 1);
+    memcpy(&num, b, sizeof(T));
+    return num;
+}
+
+template void packing::num2bigendbytes<unsigned int>(unsigned int num, unsigned char b[sizeof(unsigned int)]);
+template void packing::num2bigendbytes<int>(int num, unsigned char b[sizeof(int)]);
--- src/packing.hpp
+++ src/packing.hpp
@@ -1,20 +1,13 @@
 #ifndef PACKING_H
 #define PACKING_H
-#include <array>
 
 namespace packing {
     // these methods are for converting int/float/etc to uint8_t arrays
     // *plus* doing the endianness handling.
-    void pack_float(float val, uint8_t buf[]);
-    float unpack_float(uint8_t buf[]); // should 
-    void pack_uint16(uint16_t val, uint8_t buf[]);
-    uint16_t unpack_uint16(uint8_t buf[]);
-    void pack_int16(int16_t val, uint8_t buf[]);
-    int16_t unpack_int16(uint8_t buf[]);
-    void pack_uint32(uint32_t val, uint8_t buf[]);
-    uint32_t unpack_uint32(uint8_t buf[]);
-    void pack_int32(int32_t val, uint8_t buf[]);
-    int32_t unpack_int32(uint8_t buf[]);
+    void reverse_range(unsigned char buffer[], int left, int right);
+    template <class T> void num2bigendbytes(T num,  unsigned char b[sizeof(T)]);
+    template <class T> T bigendbytes2num(unsigned char b[sizeof(T)]);
+
 };
 
 #endif
