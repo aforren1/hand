4446bf2dc1512e17fe676ff570e55a5d2effd483
Alex Forrence <aforren1@jhu.edu>
2018-01-10 14:35:38 -0500

    Match receiveData style

--- src/communication.hpp
+++ src/communication.hpp
@@ -5,7 +5,7 @@
 
 namespace communication
 {
-void _sendSample(std::array<uint8_t, 64> &tx_data);
+void sendData(std::array<uint8_t, 64> &tx_data);
 void sendSample(const std::array<float, 15> &game_sample, std::array<uint8_t, 64> &tx_data);
 void packGameSample(const std::array<float, 15> &game_sample, std::array<uint8_t, 64> &tx_data);
 void sendSample(unsigned long timestamp, int deviation, std::array<uint16_t, 20> &raw_sample, std::array<uint8_t, 64> &tx_data);
--- src/serial_comm.cpp
+++ src/serial_comm.cpp
@@ -3,7 +3,7 @@
 
 Serial.setTimeout(0); // TODO: not valid, we should just wrap this stuff up in a class
 
-void communication::_sendSample(std::array<uint8_t, 64> &tx_data)
+void communication::sendData(std::array<uint8_t, 64> &tx_data)
 {
     Serial.write(tx_data.data(), 64); // TODO: append newline? The output won't be great in the Arduino IDE anyway...
     Serial.send_now();
@@ -11,7 +11,7 @@ void communication::_sendSample(std::array<uint8_t, 64> &tx_data)
 void communication::sendSample(const std::array<float, 15> &game_sample, std::array<uint8_t, 64> &tx_data)
 {
     communication::packGameSample(game_sample, tx_data);
-    communication::_sendSample(tx_data);
+    communication::sendData(tx_data);
 
 }
 
@@ -22,7 +22,7 @@ void communication::packGameSample(const std::array<float, 15> &game_sample, std
 void communication::sendSample(unsigned long timestamp, int deviation, std::array<uint16_t, 20> &raw_sample, std::array<uint8_t, 64> &tx_data)
 {
     communication::packRawSample(timestamp, deviation, raw_sample, tx_data);
-    communication::_sendSample(tx_data);
+    communication::sendData(tx_data);
 }
 
 void communication::packRawSample(unsigned long timestamp, int deviation, const std::array<uint16_t, 20> &raw_sample, std::array<uint8_t, 64> &packed_sample)
