5d151ab8ff76df6308caec071e8f00e070591f1b
Alex Forrence <aforren1@jhu.edu>
2018-01-14 16:22:39 -0500

    Move implementation of packing utils to header

--- src/packing.hpp
+++ src/packing.hpp
@@ -1,6 +1,9 @@
 #ifndef PACKING_H
 #define PACKING_H
+#include <iostream>
 #include <array>
+#include <algorithm>
+#include <cstring>
 
 namespace packing
 {
@@ -10,6 +13,22 @@ template <class T>
 void num2bigendbytes(T num, std::array<uint8_t, sizeof(T)> &b);
 template <class T>
 T bigendbytes2num(std::array<uint8_t, sizeof(T)> &b);
+
+template <class T>
+void packing::num2bigendbytes(T num, std::array<uint8_t, sizeof(T)> &b)
+{
+    memcpy(b.data(), &num, sizeof(T));
+    std::reverse(std::begin(b), std::end(b));
+}
+
+template <class T>
+T packing::bigendbytes2num(std::array<uint8_t, sizeof(T)> &b)
+{
+    T num;
+    std::reverse(std::begin(b), std::end(b));
+    std::memcpy(&num, b.data(), sizeof(T));
+    return num;
+}
 };
 
 #endif
