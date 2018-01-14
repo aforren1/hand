ed85b0fef0cfc7fafc62fccf26f3ffbaa7addfe4
Alex Forrence <aforren1@jhu.edu>
2018-01-14 16:25:44 -0500

    Rearrange communication declarations

--- src/hid_comm.cpp
+++ src/hid_comm.cpp
@@ -9,11 +9,6 @@ void communication::sendData(std::array<uint8_t, 64> &tx_data)
 {
     RawHID.send(tx_data.data(), 1); //TODO: set to 0?
 }
-void communication::sendSample(const std::array<float, 15> &game_sample, std::array<uint8_t, 64> &tx_data)
-{
-    communication::packGameSample(game_sample, tx_data);
-    communication::sendData(tx_data);
-}
 
 void communication::packGameSample(const std::array<float, 15> &game_sample, std::array<uint8_t, 64> &tx_data)
 {
@@ -28,9 +23,9 @@ void communication::packGameSample(const std::array<float, 15> &game_sample, std
     // at this point, tx_data should have 60 bytes filled
 }
 
-void communication::sendSample(unsigned long timestamp, int deviation, std::array<uint16_t, 20> &raw_sample, std::array<uint8_t, 64> &tx_data)
+void communication::sendSample(const std::array<float, 15> &game_sample, std::array<uint8_t, 64> &tx_data)
 {
-    communication::packRawSample(timestamp, deviation, raw_sample, tx_data);
+    communication::packGameSample(game_sample, tx_data);
     communication::sendData(tx_data);
 }
 
@@ -50,6 +45,12 @@ void communication::packRawSample(unsigned long timestamp, int deviation, const 
         count += 2;
     }
     // should have 46 bytes filled by now
+}
+
+void communication::sendSample(unsigned long timestamp, int deviation, std::array<uint16_t, 20> &raw_sample, std::array<uint8_t, 64> &tx_data)
+{
+    communication::packRawSample(timestamp, deviation, raw_sample, tx_data);
+    communication::sendData(tx_data);
 }
 
 int communication::receiveData(std::array<uint8_t, 64> &rx_data)
