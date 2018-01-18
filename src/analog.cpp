f0c9cf39bc64435a83b64184adb7adb949b764a0
Alex Forrence <aforren1@jhu.edu>
2018-01-18 16:13:02 -0500

    Move analog docs to the header

--- src/analog.cpp
+++ src/analog.cpp
@@ -7,12 +7,6 @@ namespace cpin = constants::pin;
 
 ADC *adc = new ADC();
 
-/**
- * @brief Sets the ADCs to the default settings.
- * 
- * @return void 
- * 
- */
 void analog::setupADC()
 {
     adc->setResolution(cadc::resolution);
@@ -22,12 +16,6 @@ void analog::setupADC()
     adc->setReference(ADC_REFERENCE::REF_EXT);
 }
 
-/**
- * @brief Read all analog pins once.
- * 
- * @param recent_values is a reference to a std::array of type uint16_t, with length matching the number of analog pins to read (in our case, always 20). 
- *        The results of the call to analogRead are stored here.
- */
 void analog::readAllOnce(std::array<uint16_t, 20> &recent_values)
 {
     for (std::size_t i = 0; i < cpin::sensor_pins.size(); ++i)
@@ -36,11 +24,6 @@ void analog::readAllOnce(std::array<uint16_t, 20> &recent_values)
     }
 }
 
-/**
- * @brief Read the state of a single analog pin, and convert to millivolts. Primarily used during the calibration procedure.
- * 
- * @param The pin to read (e.g. A1, A2...)
- */
 float readChannelMillivolt(uint8_t channel)
 {
     // returns analog reading in millivolts
@@ -48,12 +31,6 @@ float readChannelMillivolt(uint8_t channel)
     return val * 3.3 / adc->getMaxValue(ADC_0) * 1000;
 }
 
-/**
- * @brief Convert raw channel readings to (x, y, z).
- * 
- * @param recent_values is a reference to a std::array of type uint16_t (usually the same one used by analog::readAllOnce) and length 20.
- * @param converted_values is a reference to a std::array of type float and length 15. The results of the function are stored here.
- */
 void analog::applyRotation(std::array<uint16_t, 20> &recent_values, std::array<float, 15> &converted_values)
 {
     // scale to [0, 1] and calculate x, y, z forces. (think about this hard, though...)
@@ -64,7 +41,7 @@ void analog::applyRotation(std::array<uint16_t, 20> &recent_values, std::array<f
                               (recent_values[j + 1] / cadc::max_int) * cadc::sin_rot;
         converted_values[i + 1] = (recent_values[j] / cadc::max_int) * cadc::sin_rot - // y
                                   (recent_values[j + 1] / cadc::max_int) * cadc::cos_rot;
-        converted_values[i + 2] = (recent_values[j + 2] / cadc::max_int) + (recent_values[j + 3] / cadc::max_int); // TODO: might be subtract??
+        converted_values[i + 2] = (recent_values[j + 2] / cadc::max_int) + (recent_values[j + 3] / cadc::max_int); // z. TODO: might be subtract? I tend to get values centered around -1
         j += 4;
     }
 }
--- src/analog.hpp
+++ src/analog.hpp
@@ -4,9 +4,35 @@
 
 namespace analog
 {
+/**
+ * @brief Sets the ADCs to the default settings.
+ * 
+ * @return void 
+ * 
+ */
 void setupADC();
+
+/**
+ * @brief Read all analog pins once.
+ * 
+ * @param recent_values is a reference to a std::array of type uint16_t, with length matching the number of analog pins to read (in our case, always 20). 
+ *        The results of the call to analogRead are stored here.
+ */
 void readAllOnce(std::array<uint16_t, 20> &recent_values);
+
+/**
+ * @brief Read the state of a single analog pin, and convert to millivolts. Primarily used during the calibration procedure.
+ * 
+ * @param The pin to read (e.g. A1, A2...)
+ */
 float readChannelMillivolt(uint8_t channel);
+
+/**
+ * @brief Convert raw channel readings to (x, y, z).
+ * 
+ * @param recent_values is a reference to a std::array of type uint16_t (usually the same one used by analog::readAllOnce) and length 20.
+ * @param converted_values is a reference to a std::array of type float and length 15. The results of the function are stored here.
+ */
 void applyRotation(std::array<uint16_t, 20> &recent_values, std::array<float, 15> &converted_values);
 };
 
