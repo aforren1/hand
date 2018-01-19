494c2e124f5aaea160cd5318e5835244c2dd800b
Alex Forrence <aforren1@jhu.edu>
2018-01-19 14:38:07 -0500

    Update serial

--- src/serial_comm.cpp
+++ src/serial_comm.cpp
@@ -22,13 +22,13 @@ void communication::packGameSample(const std::array<float, 15> &game_sample, std
 {
 }
 
-void communication::sendSample(unsigned long timestamp, int deviation, std::array<uint16_t, 20> &raw_sample, std::array<uint8_t, 64> &tx_data)
+void communication::sendSample(uint32_t timestamp, int16_t deviation, std::array<uint16_t, 20> &raw_sample, std::array<uint8_t, 64> &tx_data)
 {
     communication::packRawSample(timestamp, deviation, raw_sample, tx_data);
     communication::sendData(tx_data);
 }
 
-void communication::packRawSample(unsigned long timestamp, int deviation, const std::array<uint16_t, 20> &raw_sample, std::array<uint8_t, 64> &packed_sample)
+void communication::packRawSample(uint32_t timestamp, int16_t deviation, const std::array<uint16_t, 20> &raw_sample, std::array<uint8_t, 64> &packed_sample)
 {
 }
 
