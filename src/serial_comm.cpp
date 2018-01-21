76ed20725eb2240172700ec3a4462fcd3e14109c
Alex Forrence <aforren1@jhu.edu>
2018-01-21 14:43:59 -0500

    Don't play with the timeout yet (probably not efficient)

--- src/serial_comm.cpp
+++ src/serial_comm.cpp
@@ -5,7 +5,6 @@
 void communication::initComm()
 {
     Serial.begin(9600);
-    Serial.setTimeout(0); // return immediately (match HID)
 }
 
 void communication::sendSample(const std::array<float, 15> &game_sample, std::array<uint8_t, 64> &tx_data)
