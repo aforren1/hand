4d441a35ef524f526920ac7d74b9d9fdf771ea4c
Alex Forrence <aforren1@jhu.edu>
2018-01-21 14:42:04 -0500

    Working on serial

--- src/serial_comm.cpp
+++ src/serial_comm.cpp
@@ -7,29 +7,29 @@ void communication::initComm()
     Serial.begin(9600);
     Serial.setTimeout(0); // return immediately (match HID)
 }
-void communication::sendData(std::array<uint8_t, 64> &tx_data)
-{
-    Serial.write(tx_data.data(), 64);
-    Serial.send_now();
-}
+
 void communication::sendSample(const std::array<float, 15> &game_sample, std::array<uint8_t, 64> &tx_data)
 {
-    communication::packGameSample(game_sample, tx_data);
-    communication::sendData(tx_data);
-}
-
-void communication::packGameSample(const std::array<float, 15> &game_sample, std::array<uint8_t, 64> &tx_data)
-{
+    for (auto i : game_sample)
+    {
+        Serial.print(i);
+        Serial.print(' ');
+    }
+    Serial.print('\n');
 }
 
 void communication::sendSample(uint32_t timestamp, int16_t deviation, std::array<uint16_t, 20> &raw_sample, std::array<uint8_t, 64> &tx_data)
 {
-    communication::packRawSample(timestamp, deviation, raw_sample, tx_data);
-    communication::sendData(tx_data);
-}
-
-void communication::packRawSample(uint32_t timestamp, int16_t deviation, const std::array<uint16_t, 20> &raw_sample, std::array<uint8_t, 64> &packed_sample)
-{
+    Serial.print(timestamp);
+    Serial.print(' ');
+    Serial.print(deviation);
+    Serial.print(' ');
+    for (auto i : raw_sample)
+    {
+        Serial.print(i);
+        Serial.print(' ');
+    }
+    Serial.print('\n');
 }
 
 int communication::receiveData(std::array<uint8_t, 64> &rx_data)
